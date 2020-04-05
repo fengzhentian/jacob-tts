59bd09588d259b3e440c0bbc4e76423e9b921518
fengzhentian <359051902@qq.com>
2020-04-06 01:20:43 +0800

    提供文字转换语音示例页面，生成音频链接

---
+++ jacob-tts-portal/src/main/resources/static/index.js
@@ -0,0 +1,29 @@
+
+layui.use(['layer', 'element', 'form'], function() {
+    var form = layui.form;
+
+    //监听提交
+    form.on('submit(formDemo)', function(data){
+        vm.text2audio();
+        return false;
+    });
+
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            text: '',
+            audioUrl: ''
+        },
+        methods: {
+            text2audio: function () {
+                var _this = this;
+                $.ajax({
+                    method: 'GET',
+                    url: '/api/tts/text2audio?text=' + _this.text
+                }).done(function (msg) {
+                    _this.audioUrl = msg;
+                })
+            }
+        }
+    });
+});
